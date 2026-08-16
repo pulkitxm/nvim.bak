797e12f47c72fcdbcfb939b821003d752f1af7c5
Pulkit <kpulkit15234@gmail.com>
2026-08-16 18:08:43 +0530

    nvim config backup

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,38 @@
+local function open_explorer()
+  if #Snacks.picker.get({ source = "explorer" }) == 0 then
+    Snacks.explorer({ focus = false })
+  end
+end
+
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        vim.schedule(function()
+          if vim.bo.filetype == "snacks_dashboard" then
+            return
+          end
+          open_explorer()
+        end)
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "PersistenceLoadPost",
+      callback = function()
+        vim.schedule(open_explorer)
+      end,
+    })
+  end,
+}
